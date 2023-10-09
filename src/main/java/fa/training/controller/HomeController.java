7cb6cb0e127dc668a52e68bc7a8903e13bd35ed1
Phi nguyen <phi@example.com>
2023-10-09 13:15:19 +0700

    phi

---
+++ src/main/java/fa/training/controller/HomeController.java
@@ -0,0 +1,17 @@
+package fa.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "")
+public class HomeController {
+     
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String reportTotal(Model model) {
+
+        return "redirect:/report";
+    }
+}
